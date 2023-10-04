1d71be1fcb0aaf34249a8e2865a08a74ed4e4538
Ban <jim515jim@gmail.com>
2023-10-04 21:21:24 +0800

    Changes

---
+++ blog/2023-10-04-github-page-deploy 教學.md
@@ -0,0 +1,2 @@
+Error: The deploy step encountered an error: The process '/usr/bin/git' failed with exit code 128 ❌
+Deployment failed! ❌
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,21 +6,21 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
+  title: 'Ban小空間',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://ban921.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-
+  organizationName: 'Ban921', // Usually your GitHub org/user name.
+  projectName: 'ban921.github.io', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
